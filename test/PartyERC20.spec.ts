c06f45219a946fff62269d81e3847aa5d88070cc
Pedro Medina <pedromedina190@gmail.com>
2021-08-21 13:14:27 -0400

    try build

--- test/PartyERC20.spec.ts
+++ test/PartyERC20.spec.ts
@@ -29,8 +29,8 @@ describe('PartyERC20', () => {
 
   it('name, symbol, decimals, totalSupply, balanceOf, DOMAIN_SEPARATOR, PERMIT_TYPEHASH', async () => {
     const name = await token.name()
-    expect(name).to.eq('YAY Liquidity')
-    expect(await token.symbol()).to.eq('xYAY')
+    expect(name).to.eq('xPARTY')
+    expect(await token.symbol()).to.eq('xPARTY')
     expect(await token.decimals()).to.eq(18)
     expect(await token.totalSupply()).to.eq(TOTAL_SUPPLY)
     expect(await token.balanceOf(wallet.address)).to.eq(TOTAL_SUPPLY)
